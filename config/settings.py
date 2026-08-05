fe19f68634a4312fd8da607bb3afb4b56f305393
Ximena69 <xime.mtz7u7@gmail.com>
2026-08-05 14:57:21 -0600

    Ajustar configuracion de archivos static en Railway

--- config/settings.py
+++ config/settings.py
@@ -134,4 +134,5 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
