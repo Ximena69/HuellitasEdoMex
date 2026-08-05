ffcade8e0746bc2ab74858de8810bbe8906d3fa8
Ximena69 <xime.mtz7u7@gmail.com>
2026-08-05 15:58:09 -0600

    Configurar media para produccion

--- config/urls.py
+++ config/urls.py
@@ -17,11 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("principal.urls")),
-]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # Servir imágenes media en producción
+    path('media/<path:path>', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
